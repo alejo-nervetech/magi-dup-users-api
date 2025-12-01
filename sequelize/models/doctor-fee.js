c4e32b8e952822129045f8b52b7c55f778b575e2
JFernin <jericho@nervetechph.com>
2025-12-01 12:47:26 +0800

    doctor fee

---
+++ sequelize/models/doctor-fee.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const { Model } = require('sequelize');
+const { v6 } = require('uuid');
+
+module.exports = (sequelize, DataTypes) => {
+    class DoctorFees extends Model {
+        static associate(models) {
+            DoctorFees.belongsTo(models.Users, {
+                foreignKey: 'doctorId',
+                as: 'doctor',
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            });
+        }
+    }
+
+    DoctorFees.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                defaultValue: () => `df_${v6().replace(/[^\w\s]/gi, '')}`,
+            },
+            doctorId: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            caseId: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            cptCode: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            baseFee: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+            },
+            adjustment: {
+                type: DataTypes.DECIMAL(10, 2),
+                defaultValue: 0,
+                allowNull: false,
+            },
+            adjustmentType: {
+                type: DataTypes.ENUM('positive', 'negative'),
+                allowNull: true,
+            },
+            justification: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            finalFee: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'DoctorFees',
+            tableName: 'DoctorFees',
+            timestamps: true,
+        }
+    );
+    return DoctorFees;
+};
